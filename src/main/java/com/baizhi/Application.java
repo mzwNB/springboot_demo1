1331fe36fab91f07b4769dc01ea7d1ba65dd6539
mzw <mzw18737265263@126.com>
2019-04-12 11:09:10 +0800

    没啥问题

--- src/main/java/com/baizhi/Application.java
+++ src/main/java/com/baizhi/Application.java
@@ -8,5 +8,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+        System.out.println("12sa351ca3s612as6");
     }
 }
